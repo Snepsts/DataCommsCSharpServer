0a84066681fec9fd4b48fa83aad5d3878a7e489c
Michael Ranciglio <rancigliom@gmail.com>
2017-12-03 17:09:05 -0600

    Initial Commit

---
+++ DataCommsCSharpServer/CsharpServer.cs
@@ -0,0 +1,24 @@
+﻿/* CSharpServer.cs
+ * 
+ * (C) 2017 Michael Ranciglio
+ * 
+ * Prepared for CS480 at Southeast Missouri State University
+ * 
+ * Modeled after the DataCommsCppServer.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCommsCSharpServer
+{
+	class CSharpServer
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
